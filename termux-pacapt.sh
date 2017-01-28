9f04a4814c35e3ea0c67bd0b0fb22e16aa954132
Mike Robinson <mike@pyrrus.org>
2017-01-27 21:47:45 -0500

    fix

--- termux-pacapt.sh
+++ termux-pacapt.sh
@@ -3,7 +3,7 @@
 #Setup for pacapt
 
 wget -O /data/data/com.termux/files/usr/bin/pacapt \
-https://github.com/badtoyz/termux-pacapt/raw/pacapt
+https://raw.githubusercontent.com/badtoyz/termux-pacapt/master/pacapt
 
 chmod 755 /data/data/com.termux/files/usr/bin/pacapt
 
